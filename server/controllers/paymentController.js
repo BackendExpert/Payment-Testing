69d49530ebd270d1f986715240910122abc9dbc1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-05 18:48:13 +0530

    Update

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,3 +2,26 @@ const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const Payment = require('../models/Payment')
 
+exports.createPaymentIntent = async (req, res) => {
+    try{
+        const { amount } = req.body;
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+        });
+
+        const payment = new Payment({
+            amount,
+            currency: 'usd',
+            paymentIntentId: paymentIntent.id,
+            status: 'pending',
+        });
+
+        await payment.save();
+        return res.json({ Status: "Success"})
+    }
+    catch(err){
+        console.log(err)
+    }
+}
