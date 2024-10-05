c98369dcfd2ca3b856241c4d17868e92e7e990a7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-05 19:01:05 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PaymentPage from "./Pages/PaymentPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaymentPage /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/Pages/PaymentPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import axios from 'axios';
+
+const PaymentPage = () => {
+  return (
+    <div>PaymentPage</div>
+  )
+}
+
+export default PaymentPage
